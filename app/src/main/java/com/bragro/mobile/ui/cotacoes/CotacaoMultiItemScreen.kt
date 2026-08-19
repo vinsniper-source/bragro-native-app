cc684bd18bbc7b4841c81666a516b981496f89f4
Vinicius <vinsniper@gmail.com>
2026-08-19 19:13:08 -0300

    Fix: remove import inexistente ExposedDropdownMenu

--- app/src/main/java/com/bragro/mobile/ui/cotacoes/CotacaoMultiItemScreen.kt
+++ app/src/main/java/com/bragro/mobile/ui/cotacoes/CotacaoMultiItemScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenu
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
