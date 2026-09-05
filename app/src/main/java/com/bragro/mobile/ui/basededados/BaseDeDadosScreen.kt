563d7d1ffde4508d049351cefb026254894bc56f
Vinicius <vinsniper@gmail.com>
2026-09-05 13:35:03 -0300

    Fix build errors + Base de Dados overlap + Bloco/Tabela toggle

--- app/src/main/java/com/bragro/mobile/ui/basededados/BaseDeDadosScreen.kt
+++ app/src/main/java/com/bragro/mobile/ui/basededados/BaseDeDadosScreen.kt
@@ -32,7 +32,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenu
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
